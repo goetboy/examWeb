5c85685e9ba31cde2c8330e5579c93ee490759be
goetboy <32829163+goetboy@users.noreply.github.com>
2018-05-02 16:12:24 +0800

    变更

--- src/main.js
+++ src/main.js
@@ -3,21 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import '@/utils/init'
-import store from '@/store'
-import iView from 'iview'
-import '@/styles/global/index.scss'
-import '@/styles/iview/index.less'
 
 Vue.config.productionTip = false
-
-Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  store,
   router,
-  template: '<App/>',
-  components: {App}
+  components: { App },
+  template: '<App/>'
 })
--- src/router/index.js
+++ src/router/index.js
@@ -1,61 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import auth from '@/utils/auth'
-import Layout from '@/components/Layout'
-import Root from '@/modules/Root'
-import notFound from './routes/notFound'
-import home from './routes/home'
-import articles from './routes/articles'
-import login from './routes/login'
-import logout from './routes/logout'
-import iView from 'iview'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-const router = new Router({
+export default new Router({
   routes: [
     {
       path: '/',
-      component: Root,
-      children: [
-        {
-          path: '/',
-          component: Layout,
-          children: [
-            home,
-            articles
-          ],
-          meta: {
-            requiresAuth: true
-          }
-        },
-        login,
-        logout,
-        notFound
-      ]
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
-
-router.beforeEach((to, from, next) => {
-  iView.LoadingBar.start()
-
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (!auth.loggedIn()) {
-      next({
-        path: 'login',
-        query: {redirect: to.fullPath}
-      })
-    } else {
-      next()
-    }
-  } else {
-    next()
-  }
-})
-
-router.afterEach((to, from, next) => {
-  iView.LoadingBar.finish()
-})
-
-export default router
