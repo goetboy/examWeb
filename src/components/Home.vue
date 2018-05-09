67cdce943538085e1bf7f60c33d05f6089a4e507
goetboy <32829163+goetboy@users.noreply.github.com>
2018-05-09 22:38:40 +0800

    代码更新

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,19 +5,36 @@
 </template>
 
 <script>
+import { setCookie, getCookie, delCookie } from "../assets/js/cookie.js";
 export default {
-  name: 'Home',
-  data () {
+  name: "Home",
+  data() {
     return {
-      msg: '我的第一个项目'
+      msg: "我的第一个项目"
+    };
+  },
+  mounted() {
+    /*页面挂载获取保存的cookie值，渲染到页面上*/
+    let uname = getCookie("username");
+    this.name = uname;
+    /*如果cookie不存在，则跳转到登录页*/
+    if (uname == "") {
+      this.$router.push("/");
+    }
+  },
+  methods: {
+    quit() {
+      /*删除cookie*/
+      delCookie("username");
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,7 +7,7 @@
         <input type="text" placeholder="请输入用户名" v-model="username"/>
         <input type="password" placeholder="请输入密码" v-model="password" />
         <button v-on:click="login">登陆</button>
-        <span v-on="ToRegister">没有账号？马上注册</span>
+        <span v-on:click="ToRegister">没有账号？马上注册</span>
     </div>
     <div class="register-wrap" v-show="showRegister">
 <h3>注册</h3>
@@ -15,7 +15,7 @@
  <input type="text" placeholder="请输入用户名" v-model="newUsername"/>
         <input type="password" placeholder="请输入密码" v-model="newPassword" />
                 <button v-on:click="register">注册</button>
-        <span v-on="ToLogin">已有账号？马上登陆</span>
+        <span v-on:click="ToLogin">已有账号？马上登陆</span>
 
     </div>
 </form>
@@ -47,18 +47,69 @@ if(this.username == "" || this.password == "")
 {
   alert('请输入用户名/密码');
 }else{
-  //let data={'username':this.username,'password':this.password}
-  this.$ajax({
+  let data={'username':this.username,'password':this.password}
+ /*  this.$ajax({
      method: 'post',
       url: 'http://localhost/user/login',
       data: {
         'username': this.username,
         'password': this.password
       }
-  })
+  }) */
+  this.axios.get('http://localhost/user/login',data).then((result) => {
+    alert(result);
+     if(res.data == -1){
+                  this.tishi = "该用户不存在"
+                  this.showTishi = true
+              }else if(res.data == 0){
+                  this.tishi = "密码输入错误"
+                  this.showTishi = true
+              }else if(res.data == 'admin'){
+              /*路由跳转this.$router.push*/
+                  this.$router.push('/home')
+              }else{
+                  this.tishi = "登录成功"
+                  this.showTishi = true
+                  setCookie('username',this.username,1000*60)
+                  setTimeout(function(){
+                      this.$router.push('/home')
+                  }.bind(this),1000)
+              }
+  }).catch((err) => {
+    alert(err);
+  });
 }
 
+    },
+    ToRegister(){
+    this.showRegister = true
+    this.showLogin = false
+},
+ToLogin(){
+    this.showRegister = false
+    this.showLogin = true
+},register(){
+    if(this.newUsername == "" || this.newPassword == ""){
+        alert("请输入用户名或密码")
+    }else{
+        let data = {'username':this.newUsername,'password':this.newPassword}
+        this.$http.post('http://localhost/vueapi/index.php/Home/user/register',data).then((res)=>{
+            console.log(res)
+            if(res.data == "ok"){
+                this.tishi = "注册成功"
+                this.showTishi = true
+                this.username = ''
+                this.password = ''
+                 /*注册成功之后再跳回登录页*/
+                setTimeout(function(){
+                    this.showRegister = false
+                    this.showLogin = true
+                    this.showTishi = false
+                }.bind(this),1000)
+            }
+        })
     }
+}
   }
 };
 </script>
